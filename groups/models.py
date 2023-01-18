34c3f0c375a01926f3e90ac10287f4f4130fd587
Timchenko Bogdan <tbd2504@.gmail.com>
2023-01-18 15:23:51 +0200

    Create model named Group

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    created = models.DateField(auto_now_add=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'groups'
